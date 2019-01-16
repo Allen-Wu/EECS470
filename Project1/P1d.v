bfd1017caf9d46b47892bbb26740c2249bf11a3c
shiyuwu <shiyuwu@umich.edu>
2019-01-15 22:20:13 -0500

    Modify and add module for P1d.v

--- Project1/P1d.v
+++ Project1/P1d.v
@@ -2,10 +2,13 @@ module rps2(
     input [1:0] req,
     input en,
     input sel,
-    output logic [1:0] gnt
+    output logic [1:0] gnt,
+    output logic req_up
 );
     
     always_comb begin
+        if (req[1] == 1'b1 || req[0] == 1'b1) req_up = 1'b1;
+        else req_up = 1'b0;
         // Decide based on the sel bit
         if (en == 1'b0) gnt = 2'b00;
         else if (sel == 1'b1) begin
@@ -42,20 +45,17 @@ module rps4(
     end
 
     logic [3:0] gnt_temp;
+    logic [1:0] req_up_bottom;
+    logic req_up;
+    logic [1:0] sel_top;
 
-    rps2 left(.req(req[3:2]), .en(en), .sel(count[0]), .gnt(gnt_temp[3:2]));
-    rps2 right(.req(req[1:0]), .en(en), .sel(count[0]), .gnt(gnt_temp[1:0]));
+    rps2 left(.req(req[3:2]), .en(en), .sel(count[0]), .gnt(gnt_temp[3:2]), .req_up(req_up_bottom[1]));
+    rps2 right(.req(req[1:0]), .en(en), .sel(count[0]), .gnt(gnt_temp[1:0]), .req_up(req_up_bottom[0]));
+    rps2 top(.req(req_up_bottom), .en(en), .sel(count[1]), .gnt(sel_top), .req_up(req_up));
 
-    always_comb begin
-        gnt = 4'b0000;
-        if (en == 1'b1 && count[1] == 1'b1) begin
-            if (gnt_temp[3:2] == 2'b00) gnt[1:0] = gnt_temp[1:0];
-            else gnt[3:2] = gnt_temp[3:2];
-        end
-        else if (en == 1'b1 && count[1] == 1'b0) begin
-            if (gnt_temp[1:0] == 2'b00) gnt[3:2] = gnt_temp[3:2];
-            else gnt[1:0] = gnt_temp[1:0];
-        end
-    end
+    assign gnt[3] = gnt_temp[3]&sel_top[1];
+    assign gnt[2] = gnt_temp[2]&sel_top[1];
+    assign gnt[1] = gnt_temp[1]&sel_top[0];
+    assign gnt[0] = gnt_temp[0]&sel_top[0];
 
 endmodule
